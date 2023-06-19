4cfa96cc3521bdfe1324835711da98b9805482a1
vstezhko <viktoriastezhko@gmail.com>
2023-06-19 09:12:31 +0300

    feat: clear branch

--- RSS-CSS-Selectors/src/index.ts
+++
@@ -1,7 +0,0 @@
-import App from './app/app';
-import './global.scss';
-
-console.log('start');
-
-const app = new App();
-app.start();
--- RSS-CSS-Selectors/src/view/header/Header.ts
+++
@@ -1,5 +0,0 @@
-export class Header {
-    public draw(): void {
-        console.log('header');
-    }
-}
