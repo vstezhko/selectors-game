7ea76fc38637f9b97a28173ec70e0b18702df1cf
viktoriastezko <viktoriastezhko@gmail.com>
2023-06-23 18:26:39 +0200

    feat: add logic to transform html markup to game format

---
+++ RSS-CSS-Selectors/src/utils/transformToMarkup.ts
@@ -0,0 +1,34 @@
+export const transformToGameMarkup = (html: string[]): string => {
+    const newArray = html.map((tag) => {
+        // </ ... >   =>  /&gt;close      (=> /></div>;)
+        if (/<\/\w+>/g.test(tag)) {
+            tag = tag.replace(/>/g, '/&gt;close');
+        }
+
+        // </   =>   &lt;/     (=> </)
+        if (/<\//g.test(tag)) {
+            tag = tag.replace(/<\//g, '&lt;/');
+        }
+
+        // />   =>  /&gt;close      (=> /></div>;)
+        if (/\/>/g.test(tag)) {
+            tag = tag.replace(/\/>/g, '/&gt;close');
+        }
+
+        // <   =>  open&lt;      (=> <div><;)
+        if (/</g.test(tag)) {
+            tag = tag.replace(/</g, 'open&lt;');
+        }
+
+        // >   =>  &gt;      (=> >)
+        if (/>/g.test(tag)) {
+            tag = tag.replace(/>/g, '&gt;');
+        }
+
+        return tag.replace(/open/g, '<div>').replace(/close/g, '</div>');
+    });
+
+    console.log(newArray);
+
+    return newArray.join('');
+};
--- RSS-CSS-Selectors/src/view/editor/markupPane/markupPane.ts
+++ RSS-CSS-Selectors/src/view/editor/markupPane/markupPane.ts
@@ -1,10 +1,12 @@
 import { Element } from '../../../types/type';
+import { transformToGameMarkup } from '../../../utils/transformToMarkup';
 
 export class MarkupPane {
     public draw(): void {
         console.log('MarkupPane');
 
         const markupPaneContainer: Element = document.querySelector<HTMLDivElement>('.markup-pane');
+        const innerMarkup = transformToGameMarkup(['<plate>', '<apple />', '</plate>', '<plate />']);
         const markupPaneLayout = `
             <div class='pane-header'>
                 <div class='pane-header__name'>HTML Editor</div>
@@ -15,12 +17,17 @@ export class MarkupPane {
                     1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>10<br>11<br>12<br>13<br>14<br>15<br>16<br>17<br>18<br>19<br>20
                 </div>
                 <div class='pane-window__content'>
-                    <div>&lt;div class="table"&gt;<div>&lt;orange class="small" /&gt;</div>
-                        <div class=''>&lt;orange class="small" /&gt;</div>
-                        <div>&lt;pickle /&gt;</div>
-                        <div class=''>&lt;pickle /&gt;</div>
-                        <div class=''>&lt;apple class="small" /&gt;</div>
-                        <div>&lt;apple class="small" /&gt;</div>&lt;/div&gt;
+                    <div>&lt;div class="table"&gt;
+                    ${innerMarkup}
+
+<!--                    <div>&lt;orange class="small" /&gt;</div>-->
+<!--                        <div class=''>&lt;orange class="small" /&gt;</div>-->
+<!--                        <div>&lt;pickle /&gt;</div>-->
+<!--                        <div class=''>&lt;pickle /&gt;</div>-->
+<!--                        <div class=''>&lt;apple class="small" /&gt;</div>-->
+<!--                        <div>&lt;apple class="small" /&gt;</div>-->
+                        
+                        &lt;/div&gt;
                     </div>
                 </div>
             </div>
